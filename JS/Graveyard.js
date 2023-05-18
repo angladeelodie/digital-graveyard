7cadab0ebedf1fb30c7b4fc7669e6faf2d765e85
angladeelodie <43031637+angladeelodie@users.noreply.github.com>
2023-05-18 15:26:21 +0200

    before adding text

--- JS/Graveyard.js
+++ JS/Graveyard.js
@@ -158,12 +158,6 @@ export class Graveyard {
         console.log(this.scene)
 
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({
-            color: 0x00ff00
-        });
-        const cube = new THREE.Mesh(geometry, material);
-        this.scene.add(cube);
 
     }
 
